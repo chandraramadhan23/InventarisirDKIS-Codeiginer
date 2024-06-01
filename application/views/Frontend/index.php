5ceb970ca92e6eb3abb153284db294786330bb07
Muhammad Chandra Ramadhan <82149114+chandraramadhan23@users.noreply.github.com>
2024-06-01 20:36:28 +0700

    first commit

---
+++ application/views/Frontend/index.php
@@ -0,0 +1,123 @@
+      <section id="pricing" class="pricing_area pt-120 pb-130">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-25">
+                        <h4 class="title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s"> Inventarisir E-Goverment   </h4>
+                        <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.4s">Berisi Tentang Berkas-Berkas Yang Berada Di E-Goverment.</p>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row justify-content-center  ">
+                <div class="col-lg-4 col-md-8 col-sm-10  ">
+                    <div class="single_pricing text-center mt-30 wow fadeInUp" data-wow-duration="1.3s"
+                        data-wow-delay="0.2s">
+                        <h4 class="pricing_title">Pengembangan Aplikasi</h4>
+                        <span class="price"><?php echo $count1; ?> Document</span>
+                        <ul class="pricing_list">
+                            <li>Surat Masuk</li>
+                            <li>Surat Keluar</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <a href="<?php echo base_url().'index.php/Frontend/dpa'?>" class="mian-btn">Go</a>
+                    </div> <!-- single pricing -->
+                </div>
+                <div class="col-lg-4 col-md-8 col-sm-10">
+                    <div class="single_pricing text-center mt-30 active wow fadeInUp" data-wow-duration="1.3s"
+                        data-wow-delay="0.3s">
+                        <h4 class="pricing_title">Pengelola Pusat</h4>
+                        <span class="price"><?php echo $count2; ?> Document</span>
+                        <ul class="pricing_list">
+                            <li>Surat Masuk</li>
+                            <li>Surat Keluar</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <a href="<?php echo base_url().'index.php/Frontend/dpp'?>" class="mian-btn">Go</a>
+                    </div> <!-- single pricing -->
+                </div>
+                <div class="col-lg-4 col-md-8 col-sm-10">
+                    <div class="single_pricing text-center mt-30 wow fadeInUp" data-wow-duration="1.3s"
+                        data-wow-delay="0.4s">
+                        <h4 class="pricing_title">Tata Kelola</h4>
+                        <span class="price"><?php echo $count3; ?> Document</span>
+                        <ul class="pricing_list">
+                            <li>Surat Masuk</li>
+                            <li>Surat Keluar</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <a href="<?php echo base_url().'index.php/Frontend/dtk'?>" class="mian-btn">Go</a>
+                    </div> <!-- single pricing -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <section id="team" class="team_area pt-120 pb-130">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-25">
+                        <h4 class="title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Meet The Team</h4>
+                        <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.4s"></p>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row justify-content-center team_active">
+                <div class="col-lg-4 col-md-8 col-sm-10">
+                    <div class="single_team mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">
+                        <img src="<?php echo base_url().'assets/Frontend/images/fred.jpg'?>" alt="team">
+                        
+                        <div class="team_content">
+                            <h4 class="team_name"><a href="#0">Freddy Boyke Banjarnahor.</a></h4>
+                            <ul class="social">
+                                <li><a href="#0"><i class="lni lni-facebook-filled"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-twitter-original"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-linkedin-original"></i></a></li>
+                            </ul>
+                        </div>
+                    </div> <!-- single team -->
+                </div>
+                <div class="col-lg-4 col-md-8 col-sm-10">
+                    <div class="single_team mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.3s">
+                        <img src="<?php echo base_url().'assets/Frontend/images/chan.jpeg'?>" alt="team">
+                        
+                        <div class="team_content">
+                            <h4 class="team_name"><a href="#0"> Muhamad Chandra Ramadhan</a></h4>
+                            <ul class="social">
+                                <li><a href="#0"><i class="lni lni-facebook-filled"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-twitter-original"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-linkedin-original"></i></a></li>
+                            </ul>
+                        </div>
+                    </div> <!-- single team -->
+                </div>
+                <div class="col-lg-4 col-md-8 col-sm-10">
+                    <div class="single_team mt-30 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.4s">
+                        <img src="<?php echo base_url().'assets/Frontend/images/fa.jpeg'?>" alt="team">
+                        
+                        <div class="team_content">
+                            <h4 class="team_name"><a href="#0">Muhammad Iqfha Nurhidayat</a></h4>
+                        
+                            <ul class="social">
+                                <li><a href="#0"><i class="lni lni-facebook-filled"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-twitter-original"></i></a></li>
+                                <li><a href="#0"><i class="lni lni-linkedin-original"></i></a></li>
+                            </ul>
+                        </div>
+                    </div> <!-- single team -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== CONTACT PART ENDS ======-->
+    
+    <!--====== FOOTER PART START ======-->
+
+    
